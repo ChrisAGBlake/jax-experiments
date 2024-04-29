be2ed239c997a7436db5814783c7e1f46538b74f
Chris Blake <chris.a.g.blake@gmail.com>
2024-04-29 16:23:15 +1200

    added code to train the transformer - not working yet

--- transformer.py
+++ transformer.py
@@ -23,8 +23,9 @@ class DecoderOnlyTransformer(nn.Module):
     num_layers: int
     features: int
     vocab_size: int
+    num_heads: int
     dropout_rate: float = 0.1
-    num_heads: int = 8
+    
 
     def setup(self):
         self.token_embedding = nn.Embed(num_embeddings=self.vocab_size, features=self.features)
@@ -45,7 +46,8 @@ def inference():
     num_layers = 2
     features = 32
     vocab_size = 100
-    model = DecoderOnlyTransformer(num_layers=num_layers, features=features, vocab_size=vocab_size)
+    num_heads = 2
+    model = DecoderOnlyTransformer(num_layers=num_layers, features=features, vocab_size=vocab_size, num_heads=num_heads)
 
     # generate random data
     key = jax.random.PRNGKey(0)
@@ -65,29 +67,30 @@ def inference():
 
 def train():
 
+    # load tiny shakespere dataset
+    with open('data/tiny_shakespere.txt', 'r') as f:
+        text = f.read()
+        text = text.lower()
+
+    # tokenise the text, 1 token per character to keep it simple
+    unique_tokens = sorted(list(set(text)))
+    token_to_idx = {token: idx for idx, token in enumerate(unique_tokens)}
+    idx_to_token = {idx: token for token, idx in token_to_idx.items()}
+    tokens = jnp.array([token_to_idx[token] for token in text], dtype=jnp.int32)
+
     # set hyperparameters
     key = jax.random.PRNGKey(0)
     lr = 1e-4
     batch_size = 1
     n_epochs = 10
-    context_length = 128
+    context_length = 32
 
     # setup model
     num_layers = 2
-    features = 32
-    vocab_size = 100
-    model = DecoderOnlyTransformer(num_layers=num_layers, features=features, vocab_size=vocab_size)
-
-    # load tiny shakespere dataset
-    with open('data/tiny_shakespeare.txt', 'r') as f:
-        text = f.read()
-        text = text.lower()
-
-    # tokenise the text, 1 token per character to keep it simple
-    all_tokens = sorted(list(set(text)))
-    token_to_idx = {token: idx for idx, token in enumerate(all_tokens)}
-    idx_to_token = {idx: token for token, idx in token_to_idx.items()}
-    tokens = jnp.array([token_to_idx[token] for token in text], dtype=jnp.int32)
+    features = 16
+    vocab_size = len(unique_tokens)
+    num_heads = 2
+    model = DecoderOnlyTransformer(num_layers=num_layers, features=features, vocab_size=vocab_size, num_heads=num_heads)
 
     # generate training data
     x = []
@@ -97,12 +100,40 @@ def train():
         y.append(tokens[i+1:i+context_length+1])
     x = jnp.array(x)
     y = jnp.array(y)
+    data_sz = x.shape[0]
 
-    # define the loss function
+    # setup the optimiser
+    optimiser = optax.adam(lr)
+    params = model.init(key, jnp.empty((1, context_length), dtype=jnp.int32))
+    opt_state = optimiser.init(params)
+
+    # define the loss function - cross entropy
+    def loss_fn(params, inputs, labels):
+        logits = model.apply(params, inputs)
+        return -jnp.mean(jnp.sum(labels * jax.nn.log_softmax(logits), axis=-1))
 
     # define the update step
+    @jax.jit
+    def step(params, opt_state, inputs, labels):
+        loss, grads = jax.value_and_grad(loss_fn)(params, inputs, labels)
+        updates, opt_state = optimiser.update(grads, opt_state)
+        params = optax.apply_updates(params, updates)
+        return params, opt_state, loss
 
     # train for n epochs
+    for i in range(n_epochs):
+        j = 0
+        l = 0
+        while j < data_sz:
+            e = min(data_sz, j+batch_size)
+            params, opt_state, loss = step(params, opt_state, x[j:e, :], y[j:e, :])
+            print(loss)
+            l += loss
+            j = e
+        l /= int(data_sz / batch_size)
+        print(f'epoch {i}, loss: {l}')
     
 if __name__ == '__main__':
-    inference()
+    train()
+    # inference()
+    
